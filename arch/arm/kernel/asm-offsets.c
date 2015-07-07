b4c0d4801e11c47de69444be27b5f5b26da50abd
John Stultz <john.stultz@linaro.org>
2015-07-07 15:53:57 -0700

    HACK: TERRIBLE HACK: Remove the build ban on gcc 4.8
    This patch removes the gcc 4.8 build ban restriction,
    which is totally ill-advised, but allows this kernel
    tree to be built as part of an AOSP android build
    (which currently uses gcc 4.8).
    
    Hopefully this can be dropped soon when Android-M
    shows up.
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- arch/arm/kernel/asm-offsets.c
+++ arch/arm/kernel/asm-offsets.c
@@ -50,10 +50,6 @@
 #error Your compiler is too buggy; it is known to miscompile kernels.
 #error    Known good compilers: 3.3, 4.x
 #endif
-#if GCC_VERSION >= 40800 && GCC_VERSION < 40803
-#error Your compiler is too buggy; it is known to miscompile kernels
-#error and result in filesystem corruption and oopses.
-#endif
 #endif
 
 int main(void)
