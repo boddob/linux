14a526864e981469c294f1d2b33f6d70998c126e
Archit Taneja <architt@codeaurora.org>
2016-04-17 20:28:43 +0530

    drm/msm: Use correct type for physical addresses
    The u32 type used to pass the physical addresses to iommu_map can't
    accomodate 64 bit addresses. Move to dma_addr_t to ensure wrong
    addresses aren't provided to the IOMMU driver.
    
    Signed-off-by: Archit Taneja <architt@codeaurora.org>

--- drivers/gpu/drm/msm/msm_iommu.c
+++ drivers/gpu/drm/msm/msm_iommu.c
@@ -59,7 +59,7 @@ static int msm_iommu_map(struct msm_mmu *mmu, uint32_t iova,
 		return -EINVAL;
 
 	for_each_sg(sgt->sgl, sg, sgt->nents, i) {
-		u32 pa = sg_phys(sg) - sg->offset;
+		dma_addr_t pa = sg_phys(sg) - sg->offset;
 		size_t bytes = sg->length + sg->offset;
 
 		VERB("map[%d]: %08x %08x(%zx)", i, iova, pa, bytes);
