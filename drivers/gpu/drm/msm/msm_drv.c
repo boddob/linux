freedreno-zz/kernel-msm

WIP: drm/msm: shrinker support
For a first step, only purge obj->madv==DONTNEED objects.  We could be
more agressive and next try unpinning inactive objects..  but that is
only useful if you have swap.

drivers/gpu/drm/msm/msm_drv.c
@@ -195,6 +195,8 @@ static int msm_drm_uninit(struct device *dev)
 		kfree(vbl_ev);
 	}
 
+	msm_gem_shrinker_cleanup(ddev);
+
 	drm_kms_helper_poll_fini(ddev);
 
 	drm_connector_unplug_all(ddev);
@@ -353,6 +355,7 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 	}
 
 	ddev->dev_private = priv;
+	priv->dev = ddev;
 
 	priv->wq = alloc_ordered_workqueue("msm", 0);
 	priv->atomic_wq = alloc_ordered_workqueue("msm:atomic", 0);
@@ -376,6 +379,8 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 	ret = msm_init_vram(ddev);
 	if (ret)
 		goto fail;
+
+	msm_gem_shrinker_init(ddev);
 
 	switch (get_mdp_ver(pdev)) {
 	case 4:

boddob/linux

drivers/gpu/drm/msm/msm_drv.c
@@ -197,6 +197,8 @@ static int msm_unload(struct drm_device *dev)
 		kfree(vbl_ev);
 	}
 
+	msm_gem_shrinker_cleanup(dev);
+
 	drm_kms_helper_poll_fini(dev);
 
 #ifdef CONFIG_DRM_FBDEV_EMULATION
@@ -340,6 +342,7 @@ static int msm_load(struct drm_device *dev, unsigned long flags)
 	}
 
 	dev->dev_private = priv;
+	priv->dev = dev;
 
 	priv->wq = alloc_ordered_workqueue("msm", 0);
 	priv->atomic_wq = alloc_ordered_workqueue("msm:atomic", 0);
@@ -362,6 +365,8 @@ static int msm_load(struct drm_device *dev, unsigned long flags)
 	ret = msm_init_vram(dev);
 	if (ret)
 		goto fail;
+
+	msm_gem_shrinker_init(dev);
 
 	switch (get_mdp_ver(pdev)) {
 	case 4: