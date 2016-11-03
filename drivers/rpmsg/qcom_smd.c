31ad0305b87d03c243a5a037a399bcaa47d7745f
Bjorn Andersson <bjorn.andersson@linaro.org>
2016-11-03 12:33:34 -0700

    rpmsg: smd: Pick irq trigger from DT
    Rather than hard coding the irq trigger use the one specified in DT.
    
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/rpmsg/qcom_smd.c
+++ drivers/rpmsg/qcom_smd.c
@@ -1291,7 +1291,8 @@ static int qcom_smd_parse_edge(struct device *dev,
 	}
 
 	ret = devm_request_irq(dev, irq,
-			       qcom_smd_edge_intr, IRQF_TRIGGER_RISING,
+			       qcom_smd_edge_intr,
+			       IRQF_NO_SUSPEND | IRQF_SHARED,
 			       node->name, edge);
 	if (ret) {
 		dev_err(dev, "failed to request smd irq\n");
