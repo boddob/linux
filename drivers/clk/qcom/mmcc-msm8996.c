c26f244d87921ea79d767cecd1cf57f3472478db
Archit Taneja <architt@codeaurora.org>
2016-11-28 17:07:51 +0530

    HACK: clk: qcom: mmcc-8996: Tie MMAGIC_BIMC_GDSC to the MMAGIC GDSCs of multimedia blocks
    The MMAGIC_BIMC_GDSC is required to fetch from the memory. Without enabling
    this, we end up seeing a blue screen on display.
    
    Set MMAGIC BIMC GDSC as the parent for all the MMAGIC GDSCs of all the
    multimedia blocks. This isn't the ideal solution since there isn't a real
    parent-child relationship between the GDSCs. The BIMC GDSC should ideally
    be tied to the power domain of a device that manages the noc/bus clocks.
    
    Until we have device using this power domain, stick to this method.
    
    Signed-off-by: Archit Taneja <architt@codeaurora.org>

--- drivers/clk/qcom/mmcc-msm8996.c
+++ drivers/clk/qcom/mmcc-msm8996.c
@@ -2909,6 +2909,7 @@ static struct gdsc mmagic_video_gdsc = {
 		&mmagic_video_noc_cfg_ahb_clk.clkr.hw,
 	},
 	.clk_count = 4,
+	.parent = &mmagic_bimc_gdsc.pd,
 	.pwrsts = PWRSTS_OFF_ON,
 	.flags = VOTABLE,
 };
@@ -2926,6 +2927,7 @@ static struct gdsc mmagic_mdss_gdsc = {
 		&mmagic_mdss_noc_cfg_ahb_clk.clkr.hw,
 	},
 	.clk_count = 4,
+	.parent = &mmagic_bimc_gdsc.pd,
 	.pwrsts = PWRSTS_OFF_ON,
 	.flags = VOTABLE,
 };
@@ -2943,6 +2945,7 @@ static struct gdsc mmagic_camss_gdsc = {
 		&mmagic_camss_noc_cfg_ahb_clk.clkr.hw,
 	},
 	.clk_count = 4,
+	.parent = &mmagic_bimc_gdsc.pd,
 	.pwrsts = PWRSTS_OFF_ON,
 	.flags = VOTABLE,
 };
